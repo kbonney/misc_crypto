95be57126842b6225e6faaa73cd5f5bb4c44cd59
kbonney <kirkb1998@gmail.com>
2021-09-27 12:37:42 -0600

    Fixed a bug in babai(L,w) where I was not making direct reference to the basis of L in the return statement. Fixed a bug in hadamard_ration(B) where I was trying to take the nth root of a negative number - fixed by adding an absolute value on the determinant

--- babaialg/babai.py
+++ babaialg/babai.py
@@ -13,13 +13,13 @@ def babai(L,w):
     #assume L.basis is written as a matrix of column vectors
     #assume w is given in terms of standard coordinates
     #write w in terms of L.basis()
-    w_B = np.linalg.solve(L.basis.transpose(), w)
+    w_B = np.linalg.solve(L.basis.transpose(),w)
     print(w_B)
     #round entries of w_B to nearest integer
     w_B = np.rint(w_B)
     print(w_B)
     #return the lattice element represented by w_B
-    return B.transpose().dot(w_B)
+    return L.basis.transpose().dot(w_B)
 
 def hadamard_ratio(B):
     #assume B given as matrix of column basis vectors
@@ -29,23 +29,20 @@ def hadamard_ratio(B):
         V = V*np.linalg.norm(B[i])
     #calculate hadamards ratio
     n = len(B)
-    return (np.linalg.det(B) / V)**(1/n)
+    return (np.abs(np.linalg.det(B)) / V)**(1/n)
 
 b1 = np.array([137,312])
 b2 = np.array([215,-187])
 B = np.array([b1,b2])
 L = Lattice(B)
 w = np.array([53172,81743])
-print(babai(L,w))
-print(np.linalg.det(B))
-#print(hadamard_ratio(B))
 
-#b1 = np.array([137,312])
-#b2 = np.array([215,-187])
-#B = np.array([b1,b2])
-#L = Lattice(B)
-#w = np.array([53172,81743])
-#print(babai(L,w))
+
+v1 = np.array([1975,438])
+v2 = np.array([7548,1627])
+V = np.array([v1,v2])
+K = Lattice(V)
+print(babai(K,w))
 
 
 ### Bug: line 32 breaks the program. taking the nth root of that expression makes
